105502bf569ea10af9ca9c1432c21af9484ece81
Samar <yakhyaevdev@gmail.com>
2022-10-04 22:08:41 +0900

    Overlay is done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(".close-modal");
+const openModal = document.querySelectorAll(".show-modal");
+
+// Function for open modal
+const open = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// Function for close modal
+const close = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// Show Modal
+for (let i = 0; i < openModal.length; i++)
+  openModal[i].addEventListener("click", open);
+
+// Hide Modal
+closeModal.addEventListener("click", close);
+overlay.addEventListener("click", close);
